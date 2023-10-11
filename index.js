cdb4d0583ae0162d96219464fc6be622f493105f
marufbelete9@gmail.com <marufbelete9@gmail.com>
2023-10-11 15:32:04 +0300

    ready to deploy

--- index.js
+++ index.js
@@ -528,3 +528,27 @@ sequelize.sync().then(async(result)=>{
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+
+module.exports = async () => {
+  try{
+    let theDate = new Date().toString()
+    console.log('CRON JOB RAN AT ' + theDate)
+    // let mainData = JSON.parse(fs.readFileSync(__dirname + "/data/lawn/data.json"));
+    let mainData = await getLawnMasterData()
+    await deleteAllLawnCurrent()
+    await addBulkDataLawnCurrent(mainData)
+    // fs.writeFileSync(__dirname + "/data/lawn/current.json", JSON.stringify(mainData, null, 4));
+    let mailOptions = {
+      attachments: [],
+      from: process.env.FROM,
+      to: "jdbriggs81@gmail.com",
+      subject: "EAI Cron Job Ran",
+      text: `The EAI cron job ran at ${theDate}`,
+    };
+    sendmail(mailOptions).then(e=>e).catch(e=>e)
+  }
+    catch (error) {
+      console.log(error.message)
+    }
+};
