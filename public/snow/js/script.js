ae6e99162f31292fa41ae65b525306e255aa2f13
marufbelete9@gmail.com <marufbelete9@gmail.com>
2023-10-11 09:40:03 +0300

    ready to deploy

--- public/snow/js/script.js
+++ public/snow/js/script.js
@@ -7,8 +7,8 @@ $("#success").hide();
 $("#continue1").hide()
 let report = {}, picsTaken = 1;
 // const base_url="http://localhost:7000/snow"
-const base_url="https://eaievv.com"
-// const base_url="https://repl-merge.vercel.app/snow"
+// const base_url="https://eaievv.com"
+const base_url="https://repl-merge.vercel.app/snow"
 
 //------******------
 //-----PAGE 1-------
