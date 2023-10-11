e21d39da9f10a6b759aed680486668323eb0738c
marufbelete9@gmail.com <marufbelete9@gmail.com>
2023-10-11 16:00:28 +0300

    ready to deploy

--- api/cron.js
+++ api/cron.js
@@ -1,25 +1,32 @@
+const sequelize = require("../database/db");
+const Relation = require("../models/relationModel");
 const { deleteAllLawnCurrent, addBulkDataLawnCurrent } = require("../service/lawnCurrent")
 const { getLawnMasterData } = require("../service/lawnMaster")
 
-  module.exports = async () => {
-    try{
-      let theDate = new Date().toString()
-      console.log('CRON JOB RAN AT ' + theDate)
-      // let mainData = JSON.parse(fs.readFileSync(__dirname + "/data/lawn/data.json"));
-      let mainData = await getLawnMasterData()
-      await deleteAllLawnCurrent()
-      await addBulkDataLawnCurrent(mainData)
-      // fs.writeFileSync(__dirname + "/data/lawn/current.json", JSON.stringify(mainData, null, 4));
-      let mailOptions = {
-        attachments: [],
-        from: process.env.FROM,
-        to: "jdbriggs81@gmail.com",
-        subject: "EAI Cron Job Ran",
-        text: `The EAI cron job ran at ${theDate}`,
-      };
-      sendmail(mailOptions).then(e=>e).catch(e=>e)
+module.exports = async () => {
+  try{
+    Relation()
+    sequelize.sync().then(async(result)=>{
+    }).catch(error=>{
+      console.log(error)
+    })
+    let theDate = new Date().toString()
+    console.log('CRON JOB RAN AT ' + theDate)
+    // let mainData = JSON.parse(fs.readFileSync(__dirname + "/data/lawn/data.json"));
+    let mainData = await getLawnMasterData()
+    await deleteAllLawnCurrent()
+    await addBulkDataLawnCurrent(mainData)
+    // fs.writeFileSync(__dirname + "/data/lawn/current.json", JSON.stringify(mainData, null, 4));
+    let mailOptions = {
+      attachments: [],
+      from: process.env.FROM,
+      to: "jdbriggs81@gmail.com",
+      subject: "EAI Cron Job Ran",
+      text: `The EAI cron job ran at ${theDate}`,
+    };
+    sendmail(mailOptions).then(e=>e).catch(e=>e)
+  }
+    catch (error) {
+      console.log(error.message)
     }
-      catch (error) {
-        console.log(error.message)
-      }
-  };
+};
--- index.js
+++ index.js
@@ -528,33 +528,3 @@ sequelize.sync().then(async(result)=>{
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
-console.log('HELLLOLLLL ')
-
-
-module.exports = async () => {
-  try{
-    Relation()
-    sequelize.sync().then(async(result)=>{
-    }).catch(error=>{
-      console.log(error)
-    })
-    let theDate = new Date().toString()
-    console.log('CRON JOB RAN AT ' + theDate)
-    // let mainData = JSON.parse(fs.readFileSync(__dirname + "/data/lawn/data.json"));
-    let mainData = await getLawnMasterData()
-    await deleteAllLawnCurrent()
-    await addBulkDataLawnCurrent(mainData)
-    // fs.writeFileSync(__dirname + "/data/lawn/current.json", JSON.stringify(mainData, null, 4));
-    let mailOptions = {
-      attachments: [],
-      from: process.env.FROM,
-      to: "jdbriggs81@gmail.com",
-      subject: "EAI Cron Job Ran",
-      text: `The EAI cron job ran at ${theDate}`,
-    };
-    sendmail(mailOptions).then(e=>e).catch(e=>e)
-  }
-    catch (error) {
-      console.log(error.message)
-    }
-};
