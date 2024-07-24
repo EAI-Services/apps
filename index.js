381c6c103a3149d141a35516f1b194ebec8fa792
marufbelete <beletemaruf@gmail.com>
2024-07-24 12:55:24 +0300

    sendgrid set

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const sequelize = require("./database/db");
 const DotJson = require("dot-json");
 const cors = require("cors");
 const cron = require("node-cron");
+const sgMail = require("@sendgrid/mail");
 const Relation = require("./models/relationModel");
 const reportLawnService = require("./service/reportLawnService");
 const reportSnowService = require("./service/reportSnowService");
@@ -48,6 +49,10 @@ const mail = nodemailer.createTransport({
     pass: process.env.MAIL_PW,
   },
 });
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+
 
 let rq = 0,
   mailOptions;
@@ -176,7 +181,7 @@ app.get("/snow/mail", async function (req, res) {
     filename: "image4.jpg",
     content: await compressImages("./public/snow/images/Pic3.jpg"),
   };
-  sendmail();
+  sendmail(mailOptions);
   rq = 0;
 });
 
@@ -186,15 +191,10 @@ app.get("/snow/excel", reportSnowService.createExcel);
 //--------Server Fn's---------
 //----------*******-----------
 
-function sendmail() {
-  mail.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Email sent!");
-    }
-  });
+function sendmail(mailOption) {
+  return sgMail.send(mailOption)
 }
+
 
 //----------*******-----------
 //-------Admin Routes---------
@@ -415,30 +415,30 @@ cron.schedule(
     timezone: "America/Chicago",
   }
 );
-
 async function manual() {
   try {
     let theDate = new Date().toString();
     console.log("CRON JOB RAN AT " + theDate);
     // let mainData = JSON.parse(fs.readFileSync(__dirname + "/data/lawn/data.json"));
-    let mainData = await getLawnMasterData();
-    await deleteAllLawnCurrent();
-    await addBulkDataLawnCurrent(mainData);
+    // let mainData = await getLawnMasterData();
+    // await deleteAllLawnCurrent();
+    // await addBulkDataLawnCurrent(mainData);
     // fs.writeFileSync(__dirname + "/data/lawn/current.json", JSON.stringify(mainData, null, 4));
     let mailOptions = {
       attachments: [],
       from: process.env.FROM,
       to: "jdbriggs81@gmail.com",
       subject: "EAI Cron Job Ran",
-      text: `The EAI cron job ran at ${theDate}`,
+      text: `The EAI cron job ran at ${theDate}`
     };
     sendmail(mailOptions)
       .then((e) => e)
-      .catch((e) => e);
+      .catch((e) => console.log(e));
   } catch (error) {
     console.log(error.message);
   }
 }
+app.get('/testEmail',manual)
 
 //----------*******-----------
 //-------Admin Routes---------
